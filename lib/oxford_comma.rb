5a601ce59a2791e3f91c8e36e5ddafad68e63560
Michael Ballard <ballard.michael9@gmail.com>
2018-05-08 03:55:35 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   else 
-    "#{array[0, array.length-1].join(', ')} and #{array.last}"
+    "#{array[0, array.length-1].join(', ')}, and #{array.last}"
     
   end
 end
